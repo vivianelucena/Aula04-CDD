10e03f1e18af6a26c0c2456c828fb457a77ca64e
Viviane <vivianedelucenasilva@gmail.com>
2025-04-07 09:52:23 -0300

    Aula 04

---
+++ Exercicio01.py
@@ -0,0 +1,28 @@
+mes = int(input(f'Digite o número referente ao mês do seu aniversário: '))
+
+if mes < 13:
+    if mes == 1:
+        print('Janeiro')
+    elif mes == 2:
+        print('Fevereiro')
+    elif mes == 3:
+        print('Março')
+    elif mes == 4:
+        print('Abril')
+    elif mes == 5:
+        print('Maio')
+    elif mes == 6:
+        print('Junho')
+    elif mes == 7:
+        print('Julho')
+    elif mes == 8:
+        print('Agosto')
+    elif mes == 9:
+        print('Setembro')
+    elif mes == 10:
+        print('Outubro')
+    elif mes == 11:
+        print('Novembro')
+    else:
+        print('Dezembro')
+else: print('Mês inválido!')
