59fc75496b4f16e7656eca1cb5cdc6a59a782b01
Viviane <107962455+vivianelucena@users.noreply.github.com>
2025-04-08 23:23:34 -0300

    Add files via upload

--- Desafio02.py
+++ Desafio02.py
@@ -1,1 +1,7 @@
 '''Receba um número do usuário e mostre todos os números ímpares do intervalo de 1 até o número digitado'''
+
+valor = int(input(f'Digite um valor e mostrarei os números ímpares até o valor digitado: '))
+
+for a in range(1, valor + 1):       # Adiciona 1 para incluir o valor digitado
+    if a % 2 != 0:                  # Verifica se o número é ímpar
+        print(a)                    # Exibe o número ímpar
